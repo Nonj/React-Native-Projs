ba6bc8b186327e39e274a94c088127b18b35bd21
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-03-31 22:26:27 -0700

    login form

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 import {
     EMAIL_CHANGED, 
     PASSWORD_CHANGED
@@ -15,5 +17,13 @@ export const passwordChanged = (text) => {
     return {
         type: PASSWORD_CHANGED,
         payload: text
+    }
+}
+
+export const loginUser = ({email, password}) => {
+    return (dispatch) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
+            dispatch({ type: 'LOGIN_USER_SUCESS', payload: user})
+        });
     }
 }
--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Card, CardSection, Input, Button} from './common';
 import {connect} from 'react-redux';
-import {emailChanged, passwordChanged} from '../actions';
+import {emailChanged, passwordChanged, loginUser} from '../actions';
+
 
 class LoginForm extends Component {
     onEmailChange(text) {
@@ -12,6 +13,11 @@ class LoginForm extends Component {
     onPasswordChange(text) {
         // action creator
         this.props.passwordChanged(text);
+    }
+
+    onButtonPress() {
+        const {email, password} = this.props;
+        this.props.loginUser({email, password});
     }
 
 
@@ -28,7 +34,7 @@ class LoginForm extends Component {
                 </CardSection>
                 <CardSection>
                     <Input
-                        obstruct="true"
+                        obstruct={true}
                         label="Password"
                         placeholder='Password'
                         onChangeText={this.onPasswordChange.bind(this)}
@@ -36,7 +42,7 @@ class LoginForm extends Component {
                     />
                 </CardSection>
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Login
                     </Button>
                 </CardSection>
@@ -54,4 +60,4 @@ mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, {emailChanged, passwordChanged})(LoginForm);
+export default connect(mapStateToProps, {emailChanged, passwordChanged, loginUser})(LoginForm);
